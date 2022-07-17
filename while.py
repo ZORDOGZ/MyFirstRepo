db984eb1d2765e66bbb91a9d405026e23e335680
zordogz <hargobindduggal@rediff.com>
2022-07-18 01:27:09 +0530

    Fixed the while loop statement in while.py

--- while.py
+++ while.py
@@ -3,9 +3,8 @@ c=0
 while userinp=="Y" or userinp=="y":
     c+=1
     print(c)
-    userinp=input("Do you want to Continue? ")
-    if userinp!="Y" or userinp!="y":
-        break
+    userinp=input("Do you want to Continue ?")
+else:
     print("Last Line")
 
     #To check pull if it works
